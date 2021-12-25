6de8a0012da03e0c4e2f1f423b71d7e78d90f24f
Yara <112140@emmauscollege.nl>
2021-12-25 17:00:43 +0000

    1

--- api/index.js
+++ api/index.js
@@ -57,7 +57,7 @@ function echoRequest(request, response) {
 function getCategories(request, response) {
   console.log('API ontvangt /api/categories/')
   // TODO: change query to make it return categories
-  const sqlOpdracht = db.prepare('SELECT * FROM products ORDER BY name')
+  const sqlOpdracht = db.prepare('SELECT * FROM products ORDER BY categories')
   const data = sqlOpdracht.all()
   // console.log(JSON.stringify(data, null, 2))
   response.status(200).send(data)
