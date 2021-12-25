ec0a8032c065b1a8f3fbda4c18b9c9f84a0f80d9
Yara <112140@emmauscollege.nl>
2021-12-25 18:40:16 +0000

    :/

--- db/create.sql
+++ db/create.sql
@@ -9,7 +9,7 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  inhoud TEXT (15)
+  Allergie TEXT 
 );
 
 
@@ -22,7 +22,7 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Oreo Original Cookies', 'Oreo is een uniek koekje met een heerlijke vanille cremevulling', '816905633-0', '1.89');
+insert into products (name, description, code, price, Allergie) values ('Oreo Original Cookies', 'Oreo is een uniek koekje met een heerlijke vanille cremevulling', '816905633-0', '1.89', 'noten');
 
 insert into products (name, description, code, price) values ('Oreo White Chocolate', 'Oreo omhuld met een heerlijke laag witte chocolade.', '077030122-3', '2.25');
 
@@ -41,4 +41,36 @@ insert into products (name, description, code, price) values ('Oreo Lemon Cream'
 insert into products (name, description, code, price) values ('Oreo Mint', 'Oreo mint, het unieke zwarte koekje met een heerlijke mint cremevulling.', '6868294698-8', '1.49');
 
 insert into products (name, description, code, price) values ('Oreo Gingerbread', 'De feestdagen zijn natuurlijk niet compleet zonder Gingerbread Oreo Cookies! Deze kerstkoekjes zijn gevuld met een creme met Crunchy Sugar Crystals en hebben een prachtig design voor de feestdagen.', '686928467-6', '1.89');
+
+CREATE TABLE Allergie (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+insert into Allergie (id, name) values (1, 'Noten');
+insert into Allergie (id, name) values (2, 'Soja'); 
+insert into Allergie (id, name) values (3, 'Tarwe');
+insert into Allergie (id, name) values (4, 'Glutenbevattende Granen');
+insert into Allergie (id, name) values (5, 'Melk');
+
+
+
+/*
+CREATE TABLE product_allergie (
+  id INTEGER PRIMARY KEY,
+  id_koekje NUMERIC,
+  id_allergie VARCHAR(255)
+);
+
+insert into product_allergie (id, id_koekje, id_allergie) values (1, 1, 'Noten');
+insert into product_allergie (id, id_koekje, id_allergie) values (2, 2, 'Soja'); 
+insert into product_allergie (id, id_koekje, id_allergie) values (3, 3, 'Tarwe');
+insert into product_allergie (id, id_koekje, id_allergie) values (4, 4, 'Glutenbevattende Granen');
+insert into product_allergie (id, id_koekje, id_allergie) values (5, 5, 'Melk'); */
+
+
+SELECT id, products.id, id.Allergie FROM Allergie AS 'product_allergie'
+       LEFT OUTER JOIN products
+       ON products.id = product_allergie.id;
+
 
