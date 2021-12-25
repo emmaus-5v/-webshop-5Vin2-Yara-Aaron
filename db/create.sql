4c94302f48e1d2003cdb21c86909ef078b4673c5
Yara <112140@emmauscollege.nl>
2021-12-25 20:00:23 +0000

    .

--- db/create.sql
+++ db/create.sql
@@ -2,15 +2,29 @@
 -- create tables
 --
 
- DROP TABLE IF EXISTS products; 
+DROP TABLE IF EXISTS products; 
+DROP TABLE IF EXISTS allergie; 
+
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  Allergie TEXT 
+  allergieid INTEGER 
 );
+
+CREATE TABLE allergie (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  ingredient VARCHAR(255)
+);
+
+CREATE TABLE product_allergie (
+  product_id INTEGER,
+  ingredient_id INTEGER
+);
+
+
 
 
 --
@@ -22,37 +36,34 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price, Allergie) values ('Oreo Original Cookies', 'Oreo is een uniek koekje met een heerlijke vanille cremevulling', '816905633-0', '1.89', 'noten');
+insert into products (name, description, code, price, allergieid) values ('Oreo Original Cookies', 'Oreo is een uniek koekje met een heerlijke vanille cremevulling', '816905633-0', '1.89', 1);
+insert into products (name, description, code, price, allergieid) values ('Oreo White Chocolate', 'Oreo omhuld met een heerlijke laag witte chocolade.', '077030122-3', '2.25', 2 );
+insert into products (name, description, code, price, allergieid) values ('Oreo Milk Chocolate', 'Oreo omhuld met een heerlijke laag melkchocolade.', '445924201-X', '2.25',2);
+insert into products (name, description, code, price, allergieid) values ('Oreo Golden', ' Oreo golden is een uniek koekje met een heerlijke vanille cremevulling..', '693155505-7', '1.45',3);
+insert into products (name, description, code, price, allergieid) values ('Oreo Double creme', 'Oreo double stuffed, het unieke zwarte koekje met een dubbel zo dikke vanille cremevulling.', '686928463-6', '1.89',4);
+insert into products (name, description, code, price, allergieid) values ('Oreo Crunchies original', 'Jouw geliefde Oreo koekjes in een handig bite-size formaat. Deze biscuits zijn de perfecte verhouding tussen een Crispy bite en een zachte, romige vulling.', '492662523-7', '1.59',5);
+insert into products (name, description, code, price, allergieid) values ('Oreo Peanut Butter', 'Je vertrouwde Oreo koekje, met een verrassend lekkere vulling met pindakaassmaak.', '492687524-2','1.39',2);
+insert into products (name, description, code, price, allergieid) values ('Oreo Lemon Cream', 'Oreo Lemon Cream, het unieke koekje met een citroen cremevulling.', '696948268-5', '1.29',1);
+insert into products (name, description, code, price, allergieid) values ('Oreo Mint', 'Oreo mint, het unieke zwarte koekje met een heerlijke mint cremevulling.', '6868294698-8', '1.49',3);
+insert into products (name, description, code, price, allergieid) values ('Oreo Gingerbread', 'De feestdagen zijn natuurlijk niet compleet zonder Gingerbread Oreo Cookies! Deze kerstkoekjes zijn gevuld met een creme met Crunchy Sugar Crystals en hebben een prachtig design voor de feestdagen.', '686928467-6', '1.89',4);
 
-insert into products (name, description, code, price) values ('Oreo White Chocolate', 'Oreo omhuld met een heerlijke laag witte chocolade.', '077030122-3', '2.25');
 
-insert into products (name, description, code, price) values ('Oreo Milk Chocolate', 'Oreo omhuld met een heerlijke laag melkchocolade.', '445924201-X', '2.25');
+insert into allergie (ingredient) values ('Noten');
+insert into allergie (ingredient) values ('Soja'); 
+insert into allergie (ingredient) values ('Tarwe');
+insert into allergie (ingredient) values ('Glutenbevattende Granen');
+insert into allergie (ingredient) values ('Melk');
 
-insert into products (name, description, code, price) values ('Oreo Golden', ' Oreo golden is een uniek koekje met een heerlijke vanille cremevulling..', '693155505-7', '1.45');
-
-insert into products (name, description, code, price) values ('Oreo Double creme', 'Oreo double stuffed, het unieke zwarte koekje met een dubbel zo dikke vanille cremevulling.', '686928463-6', '1.89');
-
-insert into products (name, description, code, price) values ('Oreo Crunchies original', 'Jouw geliefde Oreo koekjes in een handig bite-size formaat. Deze biscuits zijn de perfecte verhouding tussen een Crispy bite en een zachte, romige vulling.', '492662523-7', '1.59');
-
-insert into products (name, description, code, price) values ('Oreo Peanut Butter', 'Je vertrouwde Oreo koekje, met een verrassend lekkere vulling met pindakaassmaak.', '492687524-2','1.39');
-
-insert into products (name, description, code, price) values ('Oreo Lemon Cream', 'Oreo Lemon Cream, het unieke koekje met een citroen cremevulling.', '696948268-5', '1.29');
-
-insert into products (name, description, code, price) values ('Oreo Mint', 'Oreo mint, het unieke zwarte koekje met een heerlijke mint cremevulling.', '6868294698-8', '1.49');
-
-insert into products (name, description, code, price) values ('Oreo Gingerbread', 'De feestdagen zijn natuurlijk niet compleet zonder Gingerbread Oreo Cookies! Deze kerstkoekjes zijn gevuld met een creme met Crunchy Sugar Crystals en hebben een prachtig design voor de feestdagen.', '686928467-6', '1.89');
-
-CREATE TABLE Allergie (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(255)
-);
-
-insert into Allergie (id, name) values (1, 'Noten');
-insert into Allergie (id, name) values (2, 'Soja'); 
-insert into Allergie (id, name) values (3, 'Tarwe');
-insert into Allergie (id, name) values (4, 'Glutenbevattende Granen');
-insert into Allergie (id, name) values (5, 'Melk');
-
+insert into product_allergie (product_id, ingredient_id) values(2,3,4,5);
+insert into product_allergie (product_id, ingredient_id) values(2,3,4,5);
+insert into product_allergie (product_id, ingredient_id) values(2,3,4,5);
+insert into product_allergie (product_id, ingredient_id) values(2,3);
+insert into product_allergie (product_id, ingredient_id) values(2,3,4,5);
+insert into product_allergie (product_id, ingredient_id) values(2,3,4,5);
+insert into product_allergie (product_id, ingredient_id) values(2,4,5);
+insert into product_allergie (product_id, ingredient_id) values(2,3);
+insert into product_allergie (product_id, ingredient_id) values(2,4,5);
+insert into product_allergie (product_id, ingredient_id) values(2,3);
 
 
 /*
@@ -67,10 +78,12 @@ insert into product_allergie (id, id_koekje, id_allergie) values (2, 2, 'Soja');
 insert into product_allergie (id, id_koekje, id_allergie) values (3, 3, 'Tarwe');
 insert into product_allergie (id, id_koekje, id_allergie) values (4, 4, 'Glutenbevattende Granen');
 insert into product_allergie (id, id_koekje, id_allergie) values (5, 5, 'Melk'); */
+/*
 
+SELECT name, description, code, price, ingredient 
+  FROM products, allergie AS 'product_allergien'
+    LEFT OUTER JOIN products
+       ON products.allergieid = allergie.id;
 
-SELECT id, products.id, id.Allergie FROM Allergie AS 'product_allergie'
-       LEFT OUTER JOIN products
-       ON products.id = product_allergie.id;
-
+*/
 
